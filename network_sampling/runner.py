c129a795f7d813a416406931c0e2d271e7ccf8f1
Mark Burgess <markburgess1989@hotmail.com>
2019-02-19 19:36:20 +1100

    update runner

--- network_sampling/runner.py
+++ network_sampling/runner.py
@@ -13,18 +13,20 @@ def run(f):
 	print "--- Solving Network via simple statistics"
 	os.system("python stat_solver.py {}.json stat{}out.json 20 510 5 100 --inds_data=inds{}.json".format(f,f,f))
 	print "--- Solving Network via stratified statistics:BURGESS"
-	os.system("python bounds_solver.py burgess {}.json bound{}out.json 180 980 10 100 --inds_data=inds{}.json".format(f,f,f))
+	os.system("python bounds_solver.py burgess {}.json bound_burgess{}out.json 180 980 10 100 --inds_data=inds{}.json".format(f,f,f))
 	print "--- Solving Network via stratified statistics:SIMPLE"
 	os.system("python bounds_solver.py simple {}.json bound_simple{}out.json 180 1700 10 100 --inds_data=inds{}.json".format(f,f,f))
 	print "--- Solving Network via stratified statistics:MALEKI"
-	os.system("python bounds_solver.py maleki {}.json bound_simple{}out.json 180 1700 10 100 --inds_data=inds{}.json".format(f,f,f))
+	os.system("python bounds_solver.py maleki {}.json bound_maleki{}out.json 180 1700 10 100 --inds_data=inds{}.json".format(f,f,f))
 	print "--- Solving Network via stratified statistics:CASTRO"
-	os.system("python bounds_solver.py castro {}.json bound_simple{}out.json 180 1700 10 100 --inds_data=inds{}.json".format(f,f,f))
+	os.system("python bounds_solver.py castro {}.json bound_castro{}out.json 180 1700 10 100 --inds_data=inds{}.json".format(f,f,f))
 	print "--- Collating all statistical outcomes"
 	#os.system("python data_collator.py solved{}out.json stat{}out.json bound{}out.json final{}out.json".format(f,f,f,f))
 	os.system("python data_renderer.py solved{}out.json stat{}out.json ./processed/stat{}.json".format(f,f,f))
-	os.system("python data_renderer.py solved{}out.json bound{}out.json ./processed/bound{}.json".format(f,f,f))
+	os.system("python data_renderer.py solved{}out.json bound_burgess{}out.json ./processed/burgess{}.json".format(f,f,f))
 	os.system("python data_renderer.py solved{}out.json bound_simple{}out.json ./processed/simple{}.json".format(f,f,f))
+	os.system("python data_renderer.py solved{}out.json bound_maleki{}out.json ./processed/maleki{}.json".format(f,f,f))
+	os.system("python data_renderer.py solved{}out.json bound_castro{}out.json ./processed/castro{}.json".format(f,f,f))
 
 
 if __name__ == '__main__':
