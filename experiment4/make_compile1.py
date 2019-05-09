18af06af86abac4db556e568f1de3decbd3eb9a8
markopolo141 <markopolo141@github.com>
2019-05-09 13:08:10 +1000

    update

--- experiment4/make_compile1.py
+++ experiment4/make_compile1.py
@@ -1,5 +1,5 @@
 
-with open("compile2.sh","w") as f:
+with open("compile1.sh","w") as f:
 	f.write("cd ./k_generator\n");
 	f.write("./compile.sh\n");
 	f.write("cd ..\n");
@@ -7,4 +7,4 @@ with open("compile2.sh","w") as f:
 		for j in range(1,6):
 			f.write("python network_generator.py {} {} {}\n".format(i,(int)(i*6.0/5),j))
 		for j in range(1,6):
-			f.write("python timed_bounds_solver.py {}.json test_out2.json 8 180\n".format(j))
+			f.write("python bounds_solver.py {}.json test_out.json 8 0.01 10\n".format(j))
